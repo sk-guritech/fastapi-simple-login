0684e6a59d209101d1347c7024476ad477683bf0
@GuriTech <guri95saka22@gmail.com>
2022-06-26 12:35:23 -0400

    [add]added test_env

---
+++ test_env/initdb.d/initial_db.sql
@@ -0,0 +1,15 @@
+USE database;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    ulid VARCHAR(26) PRIMARY KEY,
+    bcrypt_hash VARCHAR(60) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users VALUES (
+    '01G5EXPGEREF4Q9Q8NKQPJ3BBT',
+    '$2b$12$BYZUTA9z/RBNBaO04lUuEuMEvJnm3hQlnWqu6pUUiZNBIZy9kBtyy',
+    'johndoe'
+);
---
+++ test_env/main.py
@@ -0,0 +1,36 @@
+# flake8: noqa: E402
+from __future__ import annotations
+
+import sys
+sys.path.append('.')  # noqa: E402
+
+import uvicorn
+from fastapi import Depends
+from fastapi import FastAPI
+from redis import Redis
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from fastapi_simple_login import SimpleLoginAPI  # type: ignore
+from fastapi_simple_login import UserModel
+
+
+app = FastAPI()
+
+redis = Redis('redis', 6379, 0)
+engine = create_engine('mysql://user:password@db/database')
+SessionMaker = sessionmaker(engine)
+
+SimpleLoginAPI.set_config(redis_session=redis, database_session_maker=SessionMaker, user_model=UserModel)
+SimpleLoginAPI.set_exception_handlers(app)
+
+app.include_router(SimpleLoginAPI.get_api_router())
+
+
+@app.get('/')
+async def read_root(ulid=Depends(SimpleLoginAPI.validate_access_token)):
+    return {'ulid': ulid}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, debug=True)
