2822008b0934d4e1bdcd8341d55e73ef3c1f165c
@GuriTech <guri95saka22@gmail.com>
2022-06-26 12:44:23 -0400

    [update]added test cases

--- test/test_fastapi_simple_login.py
+++ test/test_fastapi_simple_login.py
@@ -90,3 +90,23 @@ def test_validate_access_token():
     res = requests.get('http://127.0.0.1:8000/', headers={'Authorization': 'Bearer ' + access_token})
     assert res.status_code == 200
     assert res.json() == {'ulid': '01G5EXPGEREF4Q9Q8NKQPJ3BBT'}
+
+
+def test_validate_access_token_with_invalid_token():
+    res = requests.post('http://127.0.0.1:8000/login', data={'username': 'johndoe', 'password': 'secret'})
+    access_token = res.json()['access_token']
+    access_token = list(access_token)
+    access_token[0] = access_token[1]
+
+    res = requests.get('http://127.0.0.1:8000/', headers={'Authorization': 'Bearer ' + ''.join(access_token)})
+    assert res.status_code == 401
+    assert res.headers['WWW-Authenticate'] == 'Bearer error="invalid_token"'
+
+
+def test_validate_access_token_with_insufficient_scope():
+    res = requests.post('http://127.0.0.1:8000/login', data={'username': 'johndoe', 'password': 'secret'})
+    refresh_token = res.json()['refresh_token']
+
+    res = requests.get('http://127.0.0.1:8000/', headers={'Authorization': 'Bearer ' + refresh_token})
+    assert res.status_code == 403
+    assert res.headers['WWW-Authenticate'] == 'Bearer error="insufficient_scope"'
